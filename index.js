510b61b6522c271c87d0b62ec5035399d1ebde83
sprince0031 <siddharthprince31@gmail.com>
2020-05-13 02:14:13 +0530

    Almost complete except for mouseup flash action.

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
-document.querySelectorAll('.instrument-btn').forEach(item => {
-    item.addEventListener('click', event => {
-      console.log("clicked!");
-    })
-  })
+// document.querySelectorAll('.instrument-btn').forEach(item => {
+  
+  
+//   let soundFile;
+//   let audio;
+//   item.addEventListener('click', event => {
+    
+//     soundFile = soundPicker(item.getAttribute("id"));
+//     item.style.color = "white";
+//     audio = new Audio(soundFile);
+//     audio.play();
+//   });
+
+// });
+
+function soundPlayer(id) {
+
+  switch (id) {
+    case 'w':
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case 'a':
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case 's':
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case 'd':
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case 'j':
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case 'k':
+      let kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case 'l':
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    default:
+      console.log(id);
+  }
+
+}
+
+let buttonElementList = document.querySelectorAll(".instrument-btn");
+
+for (let i = 0; i < buttonElementList.length; i++) {
+  buttonElementList[i].addEventListener('click', function() {
+    let keyId = this.getAttribute("id");
+    colourOn(keyId);
+    soundPlayer(keyId);
+    
+  });
+
+  buttonElementList[i].addEventListener('onmouseup', function() {
+    colourOff(this.getAttribute("id"));
+  });
+}
+
+let keyboardShortcutToggle = true;
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'h') {
+    if (keyboardShortcutToggle) {
+      document.querySelector(".hidden").classList.toggle("reveal");
+    } else {
+      document.querySelector(".reveal").classList.toggle("hidden");
+    }
+  } else {
+    soundPlayer(event.key);
+    colourOn(event.key);
+  }
+});
+
+document.addEventListener('keyup', event => {
+    colourOff(event.key);
+});
+
+function colourOn(id) {
+  document.querySelector("#"+id).classList.add("instrument-btn-clicked");
+}
+
+function colourOff(id) {
+  document.querySelector("#"+id).classList.remove("instrument-btn-clicked");
+}
